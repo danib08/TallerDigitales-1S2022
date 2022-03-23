ba8b251d36aece8eb3382aa8f3057d7125b04179
FALCH2000 <faleiva.ch@gmai.com>
2022-03-23 16:14:52 -0600

    highLevel creation

---
+++ Lab 3/Exp 1/highLevel.sv
@@ -0,0 +1,9 @@
+module highLevel #(parameter n = 4) (input [n-1:0] a,b, input [3:0] operacion, output [6:0] display, output [3:0] flagsResult);
+	logic [n-1:0] resultado;
+	
+	calculadora #(4)calcu(a,b,operacion, resultado, flagsResult);
+	
+	decodificador deco(resultado, display);
+	
+
+endmodule
