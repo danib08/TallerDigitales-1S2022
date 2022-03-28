57294dff7da7f1ac9c9c9757e8e195694e28be1e
Carmen Valentina Araya <49073400+varaya64@users.noreply.github.com>
2022-03-27 21:20:31 -0600

    siiiiuuuuu

--- Lab 3/Exp 2/registroCargaInput.sv
+++ Lab 3/Exp 2/registroCargaInput.sv
@@ -1,6 +1,6 @@
 module registroCargaInput #(parameter n = 4) (input [n-1:0] a, b, input clk, rst, btn, output [(2*n)-1:0] s);
 	
-	logic saux = 0;
+	reg [(n*2)-1:0] saux;
 	
 	always @ (posedge clk, posedge rst, posedge btn) begin
 		if (rst == 1) begin
--- Lab 3/Exp 2/registroCargaOutput.sv
+++ Lab 3/Exp 2/registroCargaOutput.sv
@@ -1,6 +1,6 @@
 module registroCargaOutput #(parameter n = 4) ( input [n-1:0] s, input clk, rst, output [n-1:0] r);
 	
-	logic raux = 0;
+	reg [n-1:0] raux;
 	
 	always @ (negedge clk, posedge rst) begin
 		if (rst == 1) begin
