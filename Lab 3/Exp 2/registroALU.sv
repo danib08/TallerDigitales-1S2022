0447a9c98be4ab2a396c89be82d9d8c124255ab5
Carmen Valentina Araya <49073400+varaya64@users.noreply.github.com>
2022-03-29 10:07:24 -0600

    Cambios

--- Lab 3/Exp 2/registroALU.sv
+++ Lab 3/Exp 2/registroALU.sv
@@ -1,4 +1,4 @@
-module registroALU #(parameter n = 4) (input [n-1:0] a, b, input [3:0] operacion, output [6:0] display, input clk, rst, btn);
+module registroALU #(parameter n = 4 ) (input [n-1:0] a, b, input [3:0] operacion, output [6:0] display, input clk, rst, btn);
 	
 	logic [n-1:0] saux;
 	
