fad043be65b13ae5c70ecbb1027e414cfc123791
Daniela Brenes <danibreo6@gmail.com>
2022-04-27 10:46:47 -0600

    avance vga

--- Lab 4/vga_test.sv
+++ Lab 4/vga_test.sv
@@ -1,9 +1,29 @@
 module vga_test ();
 
-logic clk50MHz;
-logic clk25MHz;
-logic vga_hs;
-logic vga_vs;
-logic [7:0] vga_r;
-logic [7:0] vga_g;
-logic [7:0] vga_b;
+	logic clk50MHz;
+	logic clk25MHz;
+	logic vga_hs;
+	logic vga_vs;
+	logic [7:0] vga_r;
+	logic [7:0] vga_g;
+	logic [7:0] vga_b;
+
+	vga_toplevel controller(clk50MHz, clk25MHz, vga_hs, vga_vs, vga_r, vga_g, vga_b);
+	
+	initial begin
+		clk50MHz = 0;
+		clk25MHz = 0;
+	end
+	
+	always begin
+		clk50MHz = ~clk50MHz; 
+		#50;	
+	end
+	
+	always begin
+		clk25MHz = ~clk25MHz; 
+		#100;
+	end
+	
+endmodule
+
