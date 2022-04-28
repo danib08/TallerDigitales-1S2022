2805c8a7b6bafcd8a1a13fcf03d742d614ef602a
FALCH2000 <faleiva.ch@gmail.com>
2022-04-28 12:30:32 -0600

    controles de juego update

--- Lab 4/controlesJuego.sv
+++ Lab 4/controlesJuego.sv
@@ -1,25 +1,69 @@
-module controlesJuego(input rst, clk, btnLeft, btnRight, btnUp, btnDown,btnSelect, contadorL, output x);
-	logic [1:0] posXactual;
-	logic [1:0] posYactual;
-	logic [1:0] posXsig;
-	logic [1:0] posYsig;
+module controlesJuego(input rst, clk, btnLeft, btnRight, btnUp, btnDown,btnSelect, contadorL, input reg [2:0] memoryGame [0:3][0:3], output x);
+	logic posXactual;
+	logic posYactual;
+	logic posXsig=0;
+	logic posYsig=0;
+	logic [2:0] xAux;
+	
+	
+	// memoryGame [0:3][0:3]
 	
 	
 	always @(negedge btnLeft, negedge btnRight, posedge contadorL)
 	begin
+	
 		posXactual= posXsig;
+		
 		if(contadorL==0) begin
 			case(posXactual)
-				2'b00:
-					if(btnLeft) posXsig= 2'b00;
-					else if (btnRight) posXsig= 2'b01;
-				2'b01:
-					if(btnLeft) posXsig= 2'b00;
-					else if (btnRight) posXsig= 2'b01;
-				
+				0:
+					if(btnLeft) posXsig= 0;
+					else if (btnRight) posXsig= 1;
+				1:
+					if(btnLeft) posXsig= 0;
+					else if (btnRight) posXsig= 2;
+				2:
+					if(btnLeft) posXsig= 1;
+					else if (btnRight) posXsig= 3;
+				3:
+					if(btnLeft) posXsig= 2;
+					else if (btnRight) posXsig= 3;
+				default: posXsig=0;
 			endcase
+			
 		end
 	end
+	
+	
+	always @(negedge btnUp, negedge btnDown, posedge contadorL)
+	begin
+	
+		posYactual= posYsig;
+		
+		if(contadorL==0) begin
+			case(posYactual)
+				0:
+					if(btnUp) posYsig= 0;
+					else if (btnDown) posYsig= 1;
+				1:
+					if(btnUp) posXsig= 0;
+					else if (btnDown) posYsig= 2;
+				2:
+					if(btnUp) posXsig= 1;
+					else if (btnDown) posYsig= 3;
+				3:
+					if(btnUp) posXsig= 2;
+					else if (btnDown) posYsig= 3;
+			endcase
+			
+		end
+	end
+	
+	always @(negedge btnSelect) begin
+		xAux= memoryGame[posXactual][posYactual];
+	end
+	
+	assign x=xAux;
 	
 
 endmodule
