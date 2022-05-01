819bb9bc2a3485c1cc6e37df3bbe83f6679c64a8
FALCH2000 <faleiva.ch@gmail.com>
2022-05-01 15:56:12 -0600

    contador

--- Lab 4/contador.sv
+++ Lab 4/contador.sv
@@ -1,3 +1,11 @@
-module contador();
-
+module contador(input rst, j, output salida);
+	
+	always_ff @(posedge rst, posedge j) begin
+		if(rst)
+			salida=0;
+		else
+			salida=salida+1;
+	end
+	
+	
 endmodule
