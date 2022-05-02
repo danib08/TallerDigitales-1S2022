84d0e78b142fb0722c1a9899941b9fc707c32517
FALCH2000 <faleiva.ch@gmail.com>
2022-05-02 02:14:34 -0600

    test prueba

--- Lab 4/controlesJuego.sv
+++ Lab 4/controlesJuego.sv
@@ -1,31 +1,33 @@
-module controlesJuego(input rst, clk, btnLeft, btnRight, btnUp, btnDown,btnSelect, contadorL, input reg [3:0] memoryGame [0:3][0:3], output x);
+module controlesJuego(input rst, clk, btnLeft, btnRight, btnUp, btnDown,btnSelect, contadorL, output x);
 	logic posXactual = 0;
 	logic posYactual = 0;
 	logic posXsig = 0;
 	logic posYsig = 0;
 	logic [3:0] xAux;
-	/**
-	reg [2:0] memoryGame [0:3][0:3];
-		
+	
+	reg [3:0] memoryGame [0:3][0:3];
+
 	
 	always @ (negedge rst) begin
-		memoryGame[0][0] = 3'b000;
-		memoryGame[0][1] = 3'b011;
-		memoryGame[0][2] = 3'b111;
-		memoryGame[0][3] = 3'b010;
-		memoryGame[1][0] = 3'b110;
-		memoryGame[1][1] = 3'b100;
-		memoryGame[1][2] = 3'b001;
-		memoryGame[1][3] = 3'b111;
-		memoryGame[2][0] = 3'b001;
-		memoryGame[2][1] = 3'b101;
-		memoryGame[2][2] = 3'b111;
-		memoryGame[2][3] = 3'b000;
-		memoryGame[3][0] = 3'b100;
-		memoryGame[3][1] = 3'b010;
-		memoryGame[3][2] = 3'b101;
-		memoryGame[3][3] = 3'b011;
-	end **/
+	
+		memoryGame[0][0] = 4'b0000;
+		memoryGame[0][1] = 4'b0011;
+		memoryGame[0][2] = 4'b0111;
+		memoryGame[0][3] = 4'b0010;
+		memoryGame[1][0] = 4'b0110;
+		memoryGame[1][1] = 4'b0100;
+		memoryGame[1][2] = 4'b0001;
+		memoryGame[1][3] = 4'b0111;
+		memoryGame[2][0] = 4'b0001;
+		memoryGame[2][1] = 4'b0101;
+		memoryGame[2][2] = 4'b0111;
+		memoryGame[2][3] = 4'b0000;
+		memoryGame[3][0] = 4'b0100;
+		memoryGame[3][1] = 4'b0010;
+		memoryGame[3][2] = 4'b0101;
+		memoryGame[3][3] = 4'b0011;
+	
+	end
 	
 	
 	always @(btnLeft, btnRight, contadorL)
@@ -69,7 +71,9 @@ module controlesJuego(input rst, clk, btnLeft, btnRight, btnUp, btnDown,btnSelec
 	
 	always @(clk) begin
         xAux = memoryGame[posXactual][posYactual];
-		  $display("%b",xAux);
+		  xAux[3]=1;
+		  $display("aux %b",xAux);
+		  $display(" aux b %b",xAux[3]);
    end
 	
 	assign x = xAux;
