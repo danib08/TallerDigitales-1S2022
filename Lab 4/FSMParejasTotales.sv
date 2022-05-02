39e18c68f952f924f9e47f18c623bc92bba0dbb5
Carmen Valentina Araya <49073400+varaya64@users.noreply.github.com>
2022-05-02 01:21:14 -0600

    maaaaas logros

--- Lab 4/FSMParejasTotales.sv
+++ Lab 4/FSMParejasTotales.sv
@@ -1,14 +1,14 @@
-module FSMParejasTotales(input rst, clk, j, m, output l);
+module FSMParejasTotales(input clk, rst, j, m, output l);
 
 	logic [3:0] contador; 
 	logic [1:0] estadoActual;
-	logic [1:0] estadoSiguiente = 2'b00;
+	logic [3:0] estadoSiguiente = 4'b0000;
 	//logic [2:0] cartaActual=0;
 	logic lAux=0;
 	
 	always_ff @(posedge clk or posedge rst)
 		if(rst) 
-			estadoActual=0;
+			estadoActual=4'b0000;
 		else
 			estadoActual= estadoSiguiente;
 	
@@ -16,70 +16,70 @@ module FSMParejasTotales(input rst, clk, j, m, output l);
 		$display("Contador", contador);
 		case(estadoActual)
 			0: begin
-					if(j==1) estadoSiguiente=1;
-					else estadoSiguiente=0;
+					if(j==1) estadoSiguiente= 4'b0001;
+					else estadoSiguiente=4'b0000;
 					contador = 4'b0000;
 					lAux=0;
 					
 				end
 			1: begin
-					if(j==1) estadoSiguiente=2;
-					else estadoSiguiente=1;
+					if(j==1) estadoSiguiente=4'b0010;
+					else estadoSiguiente=4'b0001;
 					contador =4'b0001;
 					lAux=0;
 					
 				end
 			2: begin
-					if(j==1) estadoSiguiente=3;
-					else estadoSiguiente=2;
+					if(j==1) estadoSiguiente=4'b0011;
+					else estadoSiguiente=4'b0010;
 					contador = 4'b0010;
 					lAux=0;
 					
 				end	
 			3: begin
-					if(j==1) estadoSiguiente=4;
-					else estadoSiguiente=3;
+					if(j==1) estadoSiguiente=4'b0100;
+					else estadoSiguiente=4'b0011;
 					contador =4'b0011;
 					lAux=0;
 					
 				end	
 			4: begin
-					if(j==1) estadoSiguiente=5;
-					else estadoSiguiente=4;
+					if(j==1) estadoSiguiente=4'b0101;
+					else estadoSiguiente=4'b0100;
 					contador =4'b0100;
 					lAux=0;
 					
 				end	
 			5: begin
-					if(j==1) estadoSiguiente=6;
-					else estadoSiguiente=5;
+					if(j==1) estadoSiguiente=4'b0110;
+					else estadoSiguiente=4'b101;
 					contador =4'b0101;
 					lAux=0;
 					
 				end
 			6: begin
-					if(j==1) estadoSiguiente=7;
-					else estadoSiguiente=6;
+					if(j==1) estadoSiguiente=4'b0111;
+					else estadoSiguiente=4'b0110;
 					contador =4'b0110;
 					lAux=0;
 					
 				end	
 			7: begin
-					if(j==1) estadoSiguiente=8;
-					else estadoSiguiente=7;
+					if(j==1) estadoSiguiente=4'b1000;
+					else estadoSiguiente=4'b0111;
 					contador =4'b0111;
 					lAux=0;
 					
 				end
 			8: begin
-					if(j==1) estadoSiguiente=9;
-					else estadoSiguiente=8;
+					if(j==1) estadoSiguiente=4'b1001;
+					else estadoSiguiente=4'b1000;
 					contador =4'b1000 ;
 					lAux=0;
 					
 				end
 			9: begin
-					estadoSiguiente=9;
+					estadoSiguiente=4'b1001;
 					contador =4'b1001;
 					lAux=1;
 				end		
@@ -87,6 +87,7 @@ module FSMParejasTotales(input rst, clk, j, m, output l);
 			default: begin 
 				estadoSiguiente=0;
 				contador =4'b0000;
+				
 				lAux=0;
 				end
 		endcase	
--- Lab 4/testFSMParejas.sv
+++ Lab 4/testFSMParejas.sv
@@ -6,7 +6,7 @@ module testFSMParejas();
 	logic m;
 	
 
-	FSMParejasTotales paresjasTest (rst, clk, j, m, l);
+	FSMParejasTotales paresjasTest (clk, rst, j, m, l);
 	
 		
 	initial begin
@@ -15,16 +15,13 @@ module testFSMParejas();
 		clk = 1; rst = 0; j = 1; m = 0; #10;
 		clk = 0; rst = 0; j = 1; m = 0; #10;
 		clk = 1; rst = 0; j = 1; m = 0; #10;
-		clk = 1; rst = 0; j = 1; m = 0; #10;
 		clk = 0; rst = 0; j = 1; m = 0; #10;
-		clk = 1; rst = 0; j = 1; m = 0; #10;
-		clk = 0; rst = 0; j = 1; m = 0; #10;
-		clk = 1; rst = 0; j = 1; m = 0; #10;
 		clk = 1; rst = 0; j = 1; m = 0; #10;
 		clk = 0; rst = 0; j = 1; m = 0; #10;
 		clk = 1; rst = 0; j = 1; m = 0; #10;
 		clk = 0; rst = 0; j = 1; m = 0; #10;
 		clk = 1; rst = 0; j = 1; m = 0; #10;
+		clk = 0; rst = 0; j = 1; m = 0; #10;
 	end
 	
 endmodule
