fde927093b3262b1d6727a85502e6ddb5f2386c2
Daniela Brenes <danibreo6@gmail.com>
2022-06-10 02:40:21 -0600

    Separacion vga lista

--- Proyecto/Memoria/vga_sync.sv
+++ Proyecto/Memoria/vga_sync.sv
@@ -1,79 +1,67 @@
-`define GRAY 24'hC0C0C0
-
 module vga_sync 
 	( 
-		input logic clk_in,  	//25MHz
-		input logic reset,
+		input logic VGA_CLK_IN,  //25MHz
 		output logic video_on,
-		output logic hsync,		// horizontal sync
-		output logic vsync,		// vertical sync
-		output logic clk_out,
-		output logic [9:0] pixel_x,
-		output logic [9:0] pixel_y
+		output logic hsync,    // horizontal sync
+		output logic vsync,    // vertical sync
+		output logic[9:0] pixel_x, pixel_y,
+		output logic VGA_CLK_OUT
 	); 
-
-	//logic [7:0] red,
-	//logic [7:0] green,
-	//logic [7:0] blue
-
-	//localparam h_border_left = 145;	
-	//localparam h_border_right = 783;
-	//localparam v_border_up = 36;
-	//localparam v_border_down= 515;
+	
+	localparam h_border_left = 145;	
+	localparam h_border_right = 783;
+	localparam v_border_up = 36;
+	localparam v_border_down= 515;
 
 	// ------- Variables internas --------
-	reg [9:0] counter_x = 0;	// horizontal counter
-	reg [9:0] counter_y = 0;	// vertical counter
+	reg [9:0] counter_x = 0; // horizontal counter
+	reg [9:0] counter_y = 0; // vertical counter
 	
 	// Se manejan a R, G y B como variables internas para poder verificar que se asignan a la salidas
-	reg [7:0] r_red = 0;
-	reg [7:0] r_green = 0;
-	reg [7:0] r_blue = 0;
+	//reg [7:0] r_red = 0;
+	//reg [7:0] r_green = 0;
+	//reg [7:0] r_blue = 0;
 	
 	
 	// ------- Counters --------
-	always @(posedge clk_in) // horizontal counter
-		if (reset)
-			counter_x <= 0;
-		else
-			begin
-			    if (counter_x < 799)
-				    counter_x <= counter_x +1; // including borders
-			    else
-				    counter_x <= 0;
-			end
+	always @(posedge VGA_CLK_IN) // horizontal counter
+		begin
+			if (counter_x < 799)
+				counter_x <= counter_x +1; // including borders
+			else
+				counter_x <= 0;
+		end
 		
-	always @(posedge clk_in) // vertical counter
-		if (reset)
-			counter_x <= 0;
-		else
-            begin
-                if (counter_x == 799) 
-                    begin
-                        if (counter_y < 525)
-                            counter_y <= counter_y +1;
-                        else 
-                            counter_y <= 0;
-                    end
-            end            
+	always @(posedge VGA_CLK_IN) // vertical counter
+		begin
+			if (counter_x == 799) 
+				begin
+					if (counter_y < 525)
+						counter_y <= counter_y +1;
+					else 
+						counter_y <= 0;
+				end
+		end
+		
 		
 	// ------- Pattern -------
-	//always @(posedge clk_in)
+		//always @(posedge VGA_CLK_IN)
 		//begin
-			//{r_red, r_green, r_blue} <= `GRAY;
-		//end	
+		//	begin
+		//		{r_red, r_green, r_blue} <= `GRAY;
+		//	end	
+		//	end
 					
+		
 	// Hsync and Vsync
 	assign hsync = (counter_x > 0 && counter_x < 96) ? 1 : 0; // hsync for 96 counts
 	assign vsync = (counter_y >= 0 && counter_y < 2) ? 1 : 0; // vsync for 2 counts
-
-	// Video On
-	assign video_on = counter_x >= h_border_left && counter_x <= h_border_right && counter_y >= v_border_up && counter_y <= v_border_down;
 		
-	assign clk_out = clk_in;
 	assign pixel_x = counter_x;
 	assign pixel_y = counter_y;
-	
+	assign video_on = counter_x >= h_border_left && counter_x <= h_border_right && counter_y >= v_border_up && counter_y <= v_border_down;
+	assign VGA_CLK_OUT = VGA_CLK_IN;
+
 	// Color outputs (on/off)
 	//assign red = (counter_x >= h_border_left && counter_x <= h_border_right && counter_y >= v_border_up && counter_y <= v_border_down) ? r_red : 8'h00;
 	//assign green = (counter_x >= h_border_left && counter_x <= h_border_right && counter_y >= v_border_up && counter_y <= v_border_down) ? r_green : 8'h00;
--- Proyecto/Memoria/vga_top_level.sv
+++ Proyecto/Memoria/vga_top_level.sv
@@ -1,28 +1,19 @@
 `define GRAY 24'hC0C0C0
-`define WHITE 24'hFFFFFF
 
 module vga_top_level
-     (
-        input logic clk50MHz,
-        input logic reset,
-        output logic clk25MHz, 
-        output logic hsync, vsync,
-        output logic [7:0] vga_red,
-	   output logic [7:0] vga_green,
-	   output logic [7:0] vga_blue
+	(
+		input logic clk50MHz,
+		output logic clk25MHz, 
+		output logic hsync, vsync,
+		output logic [7:0] red,
+		output logic [7:0] green,
+		output logic [7:0] blue
 	);
 
-     localparam h_border_left = 145;	
-	localparam h_border_right = 783;
-	localparam v_border_up = 36;
-	localparam v_border_down= 515;
-
-     // signal declaration
-     logic [9:0] pixel_x, pixel_y;
-     logic video_on, pixel_tick;
-     logic [2:0] rgb_reg;
-     logic [2:0] rgb_next;
-     logic clk;
+	// signal declaration
+	logic [9:0] pixel_x, pixel_y;
+	logic video_on;
+	logic clk;
  
 	// Se manejan a R, G y B como variables internas para poder verificar que se asignan a la salidas
 	reg [7:0] r_red = 0;
@@ -33,7 +24,7 @@ module vga_top_level
      clock_divider divider(clk50MHz, clk);
 
      // instantiate vga sync circuit
-     vga_sync vsync_unit(clk, reset, video_on, hsync, vsync, clk25MHz, pixel_x, pixel_y);
+     vga_sync vsync_unit(clk, video_on, hsync, vsync, pixel_x, pixel_y, clk25MHz);
 
      // font generation circuit
      //font_print font_print_unit(clk, video_on, pixel_x, pixel_y, rgb_next);
@@ -51,9 +42,9 @@ module vga_top_level
 		end
 
      // Color outputs (on/off)
-     assign red = (pixel_x >= h_border_left && pixel_x <= h_border_right && pixel_y >= v_border_up && pixel_y <= v_border_down) ? r_red : 8'h00;
-     assign green = (pixel_x >= h_border_left && pixel_x <= h_border_right && pixel_y >= v_border_up && pixel_y <= v_border_down) ? r_green : 8'h00;
-     assign blue = (pixel_x >= h_border_left && pixel_x <= h_border_right && pixel_y >= v_border_up && pixel_y <= v_border_down) ? r_blue : 8'h00;
+     assign red = (video_on) ? r_red : 8'h00;
+     assign green = (video_on) ? r_green : 8'h00;
+     assign blue = (video_on) ? r_blue : 8'h00;
 
 
 endmodule
