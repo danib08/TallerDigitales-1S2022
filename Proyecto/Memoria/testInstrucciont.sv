19f1101f11e8722947a3c68ad05ee41163388b73
FALCH2000 <faleiva.ch@gmail.com>
2022-06-11 22:38:40 -0600

    test Register File

--- Proyecto/Memoria/testInstrucciont.sv
+++ Proyecto/Memoria/testInstrucciont.sv
@@ -1,11 +1,15 @@
 module testInstrucciont();
 
-    logic clk, reset;
+    logic clk, reset, we3;
     logic [31:0] PCNext, PC, dataOut;
+	 logic [3:0]  ra2, ra3;
+	 logic [31:0] wd3, r15;
+	 logic [31:0] rd1, rd2;
 
 	 
 	 pcCounter pcCount(clk, reset, PCNext, PC);
 	 InstructionMemory InstructionM(PC, dataOut);
+	 RegisterFile registerFile(clk, we3, dataOut[19:16], dataOut[3:0], dataOut[15:12], wd3, PC+8, rd1, rd2);
 
     initial begin 
         #1;
@@ -16,7 +20,9 @@ module testInstrucciont();
         reset = 0;
         #1;
 		  
-		  PCNext = 32'd10;
+		  PCNext = 32'd00;
+		  we3=0;
+		  
 
     end
 	 
---
+++ Proyecto/Memoria/testRegisterFile.sv
@@ -0,0 +1,8 @@
+module testRegisterFile();
+	logic clk, we3;
+	logic [3:0] ra1, ra2, ra3;
+	logic [31:0] wd3, r15, rd1, rd2;
+	
+	
+
+endmodule
