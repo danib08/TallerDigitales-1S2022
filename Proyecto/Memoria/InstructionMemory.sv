6696332591eaf72b09ea445abb1f5f9272037b1b
Daniela Brenes <danibreo6@gmail.com>
2022-06-11 19:58:07 -0600

    Cambio instruction memory

--- Proyecto/Memoria/InstructionMemory.sv
+++ Proyecto/Memoria/InstructionMemory.sv
@@ -1,17 +1,17 @@
-module InstructionMemory(input [31:0] pc, input clk, output [31:0] dataOut);
+module InstructionMemory(input [31:0] pc, output [31:0] dataOut);
 
-	logic [7:0] rom [528:0];
-	logic [31:0] dataOutAux;
+	//logic [7:0] rom [528:0];
+	logic [31:0] rom [528:0];
 	
 	initial
 		$readmemh("instructions.dat",rom);
 	
-	always @(posedge clk)
-	begin
-		dataOutAux= {rom[pc], rom[pc+1], rom[pc+2], rom[pc+3]};
-	end
+	//always @(posedge clk)
+	//begin
+	//	dataOutAux= {rom[pc], rom[pc+1], rom[pc+2], rom[pc+3]};
+	//end
 	
-	assign dataOut= dataOutAux;
+	assign dataOut = {rom[pc], rom[pc+1], rom[pc+2], rom[pc+3]}; // word aligned
 
 endmodule 
 
