7ed80232b4b91e4d178734390594e4dbfe42184a
FALCH2000 <faleiva.ch@gmail.com>
2022-06-13 21:11:57 -0600

    load button

--- Proyecto/Memoria/TopLevel.sv
+++ Proyecto/Memoria/TopLevel.sv
@@ -1,6 +1,7 @@
 module TopLevel
 	(
-		input clk, rst, 
+		input clk, rst,
+		input btnLoad,
 		output MemWrite, 
 		output logic clkVGA, hsync, vsync, 
 		output logic [7:0] red, green, blue,
@@ -10,12 +11,16 @@ module TopLevel
 	logic [31:0] Pc, instr;
 	logic [7:0] ReadData;
 	
-	processor processorARM(clk,rst, {24'b0,ReadData}, instr, Pc, MemWrite, DataAdr, WriteData);
+	always @(negedge btnLoad) begin
 	
-	InstructionMemory instructionMemory(Pc, instr);
-	
-	DataMemory Ram(DataAdr[7:0], clk, WriteData[7:0], MemWrite, ReadData);
+		processor processorARM(clk,rst, {24'b0,ReadData}, instr, Pc, MemWrite, DataAdr, WriteData);
 		
-	vga_top_level VGA(clk, rst, 8'h63, clkVGA, hsync, vsync, red, green, blue);
+		InstructionMemory instructionMemory(Pc, instr);
+		
+		DataMemory Ram(DataAdr[7:0], clk, WriteData[7:0], MemWrite, ReadData);
+			
+		//vga_top_level VGA(clk, rst, 8'h63, clkVGA, hsync, vsync, red, green, blue);
+		
+	end
 	
 endmodule
