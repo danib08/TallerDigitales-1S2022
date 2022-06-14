f70e0d0690e3abb720ab1f71012716c6e23ec7c0
Carmen Araya <valentinarayach64@gmail.com>
2022-06-13 23:50:14 -0600

    Updates

--- Proyecto/Memoria/TopLevel.sv
+++ Proyecto/Memoria/TopLevel.sv
@@ -1,10 +1,7 @@
 module TopLevel
 	(
-		input clk, rst, rstVGA,
-		input btnLoad,
+		input clk, rst,  
 		output MemWrite, 
-		output logic clkVGA, hsync, vsync, 
-		output logic [7:0] red, green, blue,
 		output logic [31:0] WriteData, DataAdr
 	);
 
@@ -12,13 +9,12 @@ module TopLevel
 	logic [7:0] ReadData;
 	
 	
-	
+
 	processor processorARM(clk,rst, {24'b0,ReadData}, instr, Pc, MemWrite, DataAdr, WriteData);
 	
 	InstructionMemory instructionMemory(Pc, instr);
 	
 	DataMemory Ram(DataAdr[7:0], clk, WriteData[7:0], MemWrite, ReadData);
 		
-	//vga_top_level VGA(clk, rstVGA, 8'h63, clkVGA, hsync, vsync, red, green, blue);
-	
+
 endmodule
